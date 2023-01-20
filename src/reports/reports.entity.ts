4ae6176c28a1b2d5a1882db0746827e6dd04995e
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-20 15:32:53 +0500

    create a used car selling app
    create an app, create services, controllers, modules and repos in it

---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+}
