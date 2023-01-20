b22b8279bd4916c47eb6b334e446cab8969f6b62
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-20 19:20:09 +0500

    feat: add a regestration feature
    add a regestratio option and validate user emaila nd password

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will strip out any extra property send in the request
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
