57583958108fa44b19577407d56db6745d20be76
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-25 15:38:26 +0500

    refactor: using interceptors and guards
    using guards to guard /whoami end-point

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,13 +9,17 @@ import {
   Patch,
   Post,
   Query,
-  Session
+  Session,
+  UseGuards
 } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guards';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -27,11 +31,9 @@ export class UsersController {
   ) {}
 
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    if (!session.userId) {
-      throw new NotFoundException('no user signed in');
-    }
-    return this.userService.findOne(session.userId);
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
