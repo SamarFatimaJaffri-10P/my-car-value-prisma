8e841a046f01f92c18643f969fb126ee2bb3ee30
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-26 16:43:30 +0500

    test: add e2e test and create a new db for test
    create an e2e tests, new test db and setup env to use different dbs for test and development environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,59 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 import { Report } from './reports/reports.entity';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
+    // config information
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`, // NODE_ENV is test in test environment and development in develop env
+    }),
     // setting up db connection
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      // using dependency injection to find the configuration services having all of our config info. from the chosen config file
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // setting up global pipe
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true, // this will strip out any extra property send in the request
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        // setting up cookie session
+        cookieSession({
+          keys: ['random_string'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const cookieSession = require('cookie-session');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // setting up cookie session
-  app.use(
-    cookieSession({
-      keys: ['random_string'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // this will strip out any extra property send in the request
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
