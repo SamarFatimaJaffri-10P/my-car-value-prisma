6c4c3be9deeeff3955d5825d896504b69a213aed
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-26 13:21:43 +0500

    test: add tests for findUser, findAllUsers and signin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,8 +12,8 @@ import {
   Session,
   UseGuards
 } from '@nestjs/common';
-import { AuthGuard } from 'src/guards/auth.guards';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthGuard } from '../guards/auth.guards';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
