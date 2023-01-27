093e9e68f6ac52b97de46991940c12855a54302e
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-27 14:49:52 +0500

    feat: implement /reports endpoint
    create a /report route and let user create report and return all report info and user id of the user that created the report

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,13 @@
 import { Exclude } from 'class-transformer';
+import { Report } from 'src/reports/reports.entity';
 import {
   AfterInsert,
   AfterRemove,
   AfterUpdate,
   Column,
   Entity,
-  PrimaryGeneratedColumn,
+  OneToMany,
+  PrimaryGeneratedColumn
 } from 'typeorm';
 
 @Entity()
@@ -19,6 +21,14 @@ export class User {
   @Column()
   @Exclude()
   password: string;
+
+  /**
+   * `() => Report` tells TypeORM that the attribute is going to be of `Report Entity type
+   *                and `() =>` is there to resolve the circular dependency issue
+   * `(report) => report.user` this is how TypeORM manage entities
+   */
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   // Hooks only work when save() and remove() is being used
   // hooks doesn't work whe insert, update, or delete is used directly
